12fe305e50307e9a19f297ade203d1e1d4651afb
Gagan Malvi <malvi@aospa.co>
2021-03-31 23:05:51 +0530

    cartyx: Initialize some printing functions
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- include/display/vga_func.h
+++ include/display/vga_func.h
@@ -1,6 +1,15 @@
 #ifndef VGA_FUNC_H
 #define VGA_FUNC_H
 
+//index for video buffer array
+uint32 vga_index;
+//counter to store new lines
+static uint32 next_line_index = 1;
+//fore & back color values
+uint8 g_fore_color = WHITE, g_back_color = BLUE;
+//digit ascii code for printing integers
+int digit_ascii_codes[10] = {0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39};
+
 /*
 16 bit video buffer elements(register ax)
 8 bits(ah) higher : 
@@ -40,6 +49,87 @@ void init_vga(uint8 fore_color, uint8 back_color)
 {
   vga_buffer = (uint16*)VGA_ADDRESS;  //point vga_buffer pointer to VGA_ADDRESS 
   clear_vga_buffer(&vga_buffer, fore_color, back_color);  //clear buffer
+  g_fore_color = fore_color;
+  g_back_color = back_color;
+}
+
+/*
+increase vga_index by width of row(80)
+*/
+void nline()
+{
+  if(next_line_index >= 55){
+    next_line_index = 0;
+    clear_vga_buffer(&vga_buffer, g_fore_color, g_back_color);
+  }
+  vga_index = 80*next_line_index;
+  next_line_index++;
+}
+
+//assign ascii character to video buffer
+void print_char(char ch)
+{
+  vga_buffer[vga_index] = vga_entry(ch, g_fore_color, g_back_color);
+  vga_index++;
+}
+
+
+uint32 strlen(const char* str)
+{
+  uint32 length = 0;
+  while(str[length])
+    length++;
+  return length;
+}
+
+uint32 digit_count(int num)
+{
+  uint32 count = 0;
+  if(num == 0)
+    return 1;
+  while(num > 0){
+    count++;
+    num = num/10;
+  }
+  return count;
+}
+
+void itoa(int num, char *number)
+{
+  int dgcount = digit_count(num);
+  int index = dgcount - 1;
+  char x;
+  if(num == 0 && dgcount == 1){
+    number[0] = '0';
+    number[1] = '\0';
+  }else{
+    while(num != 0){
+      x = num % 10;
+      number[index] = x + '0';
+      index--;
+      num = num / 10;
+    }
+    number[dgcount] = '\0';
+  }
+}
+
+//print string by calling print_char
+void strprint(char *str)
+{
+  uint32 index = 0;
+  while(str[index]){
+    print_char(str[index]);
+    index++;
+  }
+}
+
+//print int by converting it into string
+//& then printing string
+void numprint(int num)
+{
+  char str_num[digit_count(num)+1];
+  itoa(num, str_num);
+  strprint(str_num);
 }
 
 #endif
--- kernel.c
+++ kernel.c
@@ -1,22 +1,19 @@
 #include "include/display/vga_drv.h"
 #include "include/display/vga_func.h"
 
+void mainfunc();
+
 void kernel_entry()
 {
   //first init vga with fore & back colors
   init_vga(WHITE, BLACK);
+  mainfunc();
+}
 
-  //assign each ASCII character to video buffer
-  //you can change colors here
-  vga_buffer[0] = vga_entry('H', WHITE, BLACK);
-  vga_buffer[1] = vga_entry('e', WHITE, BLACK);
-  vga_buffer[2] = vga_entry('l', WHITE, BLACK);
-  vga_buffer[3] = vga_entry('l', WHITE, BLACK);
-  vga_buffer[4] = vga_entry('o', WHITE, BLACK);
-  vga_buffer[5] = vga_entry(' ', WHITE, BLACK);
-  vga_buffer[6] = vga_entry('W', WHITE, BLACK);
-  vga_buffer[7] = vga_entry('o', WHITE, BLACK);
-  vga_buffer[8] = vga_entry('r', WHITE, BLACK);
-  vga_buffer[9] = vga_entry('l', WHITE, BLACK);
-  vga_buffer[10] = vga_entry('d', WHITE, BLACK);
+void mainfunc()
+{
+  strprint("Welcome to Cartyx!");
+  nline();
+  strprint("==================");
+  nline();
 }
