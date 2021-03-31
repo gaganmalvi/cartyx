48bcc68747d8dd5a7ef4f215002d094789e847d0
Gagan Malvi <malvi@aospa.co>
2021-03-31 22:42:39 +0530

    cartyx: sandbox build
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- compile.sh
+++ compile.sh
@@ -1,20 +1,25 @@
-#assemble boot.s file
+# Sandbox build
+mkdir out
+cp -r * out
+cd out
+
+# Assemble boot.s file
 as --32 boot.s -o boot.o
 
-#compile kernel.c file
+# Compile kernel.c file
 gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
-#linking the kernel with kernel.o and boot.o files
+# Linking the kernel with kernel.o and boot.o files
 ld -m elf_i386 -T linker.ld kernel.o boot.o -o Cartyx.bin -nostdlib
 
-#check Cartyx.bin file is x86 multiboot file or not
+# Check Cartyx.bin file is x86 multiboot file or not
 grub-file --is-x86-multiboot Cartyx.bin
 
-#building the iso file
+# Building the iso file
 mkdir -p isodir/boot/grub
 cp Cartyx.bin isodir/boot/Cartyx.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o Cartyx.iso isodir
 
-#run it in qemu
+# Run it in qemu
 qemu-system-x86_64 -cdrom Cartyx.iso
