29891673b2128ffc26b0720f5ce3358b551de993
Gagan Malvi <malvi@aospa.co>
2021-03-31 23:14:52 +0530

    cartyx: Cleanup directory structure
    Signed-off-by: Gagan Malvi <malvi@aospa.co>

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,16 +1,17 @@
 # Sandbox build
+cd ../
 mkdir out
 cp -r * out
 cd out
 
 # Assemble boot.s file
-as --32 boot.s -o boot.o
+as --32 boot/boot.s -o boot.o
 
 # Compile kernel.c file
 gcc -m32 -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
 # Linking the kernel with kernel.o and boot.o files
-ld -m elf_i386 -T linker.ld kernel.o boot.o -o Cartyx.bin -nostdlib
+ld -m elf_i386 -T boot/linker.ld kernel.o boot.o -o Cartyx.bin -nostdlib
 
 # Check Cartyx.bin file is x86 multiboot file or not
 grub-file --is-x86-multiboot Cartyx.bin
@@ -18,7 +19,7 @@ grub-file --is-x86-multiboot Cartyx.bin
 # Building the iso file
 mkdir -p isodir/boot/grub
 cp Cartyx.bin isodir/boot/Cartyx.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
+cp boot/grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o Cartyx.iso isodir
 
 # Run it in qemu
